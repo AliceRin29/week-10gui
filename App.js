1322b9f6cecd8d595e5854169051e6983597b9e7
AliceRin29 <69286020+AliceRin29@users.noreply.github.com>
2020-09-10 11:20:47 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import *as  React from 'react'
+import{View,Text,Button} from 'react-native'
+import{NavigationContainer} from '@react-navigation/native'
+import{createStackNavigator} from '@react-navigation/stack'
+
+const Homescreen=({navigation})=>{
+return(
+<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+  <Text>Home Screen</Text>
+  <Button
+    title="Go to Detail" 
+    onPress={()=>navigation.navigate('Detail')}
+  />
+</View>
+  )
+}
+const DetailScreen =()=>{
+  return(
+    <View>
+      <Text> DetailScreen</Text>
+    </View>
+  )
+}
+const Stack = createStackNavigator();
+const App=()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{title:"Overview"}} component ={Homescreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+export default App;
